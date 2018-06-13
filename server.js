7511ab4bd50cde6ea15a22ea0e9b6e23153293e1
nevena29 <nevena.29@gmail.com>
2018-06-13 22:55:17 +0200

    connection with db

---
+++ server.js
@@ -0,0 +1,40 @@
+
+
+// server.js
+const mysql = require('mysql');
+
+// creating connection to the db
+const con = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'password',
+  database: 'project_adressbook',
+
+});
+
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    console.log(err);
+    
+    return;
+  }
+  console.log('Connection established');
+});
+
+
+con.query('SELECT * FROM adress_book', (err,rows) => {
+//  if(err) throw err
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+});
+
+
+
+con.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
